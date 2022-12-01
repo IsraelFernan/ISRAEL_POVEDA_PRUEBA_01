d3d773b2062e885c789fe0070e19e0f4c713d206
IsraelFernan <alexlopescado@gmail.com>
2022-12-01 18:45:46 -0500

    Funcion

--- app/src/main/java/com/poveda/pruebas/MainActivity.java
+++ app/src/main/java/com/poveda/pruebas/MainActivity.java
@@ -2,13 +2,26 @@ package com.poveda.pruebas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button siguiente;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        siguiente = findViewById(R.id.mainSiguiente);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,SegundoActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
